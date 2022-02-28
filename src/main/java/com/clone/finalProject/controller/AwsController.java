e5519619f9d605855557b1e873e93befd30fdacb
kyungwoon <nakw0410@gmail.com>
2022-02-28 21:35:04 +0900

    feat : S3 이미지 삭제 테스트

--- src/main/java/com/clone/finalProject/controller/AwsController.java
+++ src/main/java/com/clone/finalProject/controller/AwsController.java
@@ -2,6 +2,7 @@ package com.clone.finalProject.controller;
 
 import com.clone.finalProject.service.S3Uploader;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,17 +16,16 @@ public class AwsController {
 
     private final S3Uploader s3Uploader;
 
-    @PostMapping("/images")
+    @PostMapping("/images/upload")
     public String upload(@RequestParam("images") MultipartFile multipartFile) throws IOException {
         s3Uploader.upload(multipartFile, "static");
         return "test";
     }
+
+    // 사진 삭제 테스트
+    @DeleteMapping("/image/delete")
+    public String deleteimage(@RequestParam("path") String fileName) {
+        s3Uploader.deleteFile(fileName);
+        return "삭제완료";
+    }
 }
-
-//    // 사진 삭제 테스트
-//    @DeleteMapping("/image")
-//    public void deleteimage(@RequestParam String fileName) {
-//        s3Uploader.deleteFile(fileName);
-//    }
-
-
