08473f9a472b081ee6cb5278b286dcce0cafc362
kyungwoon <nakw0410@gmail.com>
2022-03-16 15:25:31 +0900

    feat: 비속어 데이터

---
+++ src/main/java/com/clone/finalProject/domain/Fword.java
@@ -0,0 +1,21 @@
+package com.clone.finalProject.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Entity
+public class Fword {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String fWord;
+}
