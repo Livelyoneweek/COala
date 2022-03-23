8c02f7a226162c98e06148bf73de0a8e5848b17a
Livelyoneweek <wlvvwhs@naver.com>
2022-03-23 18:52:30 +0900

    0323 -edit 자동배포 test

--- src/main/java/com/clone/finalProject/service/AnswerService.java
+++ src/main/java/com/clone/finalProject/service/AnswerService.java
@@ -1,8 +1,9 @@
 package com.clone.finalProject.service;
 
 import com.clone.finalProject.domain.*;
-import com.clone.finalProject.dto.*;
-
+import com.clone.finalProject.dto.AlarmResponseDto;
+import com.clone.finalProject.dto.AnswerResponseDto;
+import com.clone.finalProject.dto.CommnetResponseDto;
 import com.clone.finalProject.repository.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,7 +12,6 @@ import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 @Slf4j
