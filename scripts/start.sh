f6c942d2386fac045a89d28c7ae9937fc6e9298f
Livelyoneweek <wlvvwhs@naver.com>
2022-03-26 11:56:32 +0900

    0326 -edit 무중단배포 test

--- scripts/start.sh
+++ scripts/start.sh
@@ -43,10 +43,8 @@ IDLE_PROFILE=$(find_idle_profile)
 
 echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 
-#nohup java -jar -Dspring.profiles.active=$IDLE_PROFILE week03-0.0.1-SNAPSHOT.jar
-# classpath:/application.properties 는 프로젝트 내부 안에 프로퍼티스가 없으므로 제외해야함
 nohup java -jar \
-    -Dspring.config.location=classpath:/application-$IDLE_PROFILE.properties \
+    -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties \
     -Dspring.profiles.active=$IDLE_PROFILE \
     $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
 
