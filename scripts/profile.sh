f10bfce0b3315103cb344162bde2558e732bf29d
Livelyoneweek <wlvvwhs@naver.com>
2022-03-27 22:22:47 +0900

    0327 -edit 무중단배포 test

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -7,21 +7,21 @@ function find_idle_profile()
 {
     RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" https://ab.choideveloper.shop/profile)
 
-#    CURRENT_PROFILE=$(curl -s https://ab.choideveloper.shop/profile)
+    CURRENT_PROFILE=$(curl -s https://ab.choideveloper.shop/profile)
 
-    if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면 (즉, 40x/50x 에러 모두 포함)
-    then
-        CURRENT_PROFILE=real2
-    else
-        CURRENT_PROFILE=$(curl -s https://ab.choideveloper.shop/profile)
-    fi
-
-    if [ ${CURRENT_PROFILE} == real1 ]
-    then
-      IDLE_PROFILE=real2
-    else
-      IDLE_PROFILE=real1
-    fi
+#    if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면 (즉, 40x/50x 에러 모두 포함)
+#    then
+#        CURRENT_PROFILE=real2
+#    else
+#        CURRENT_PROFILE=$(curl -s https://ab.choideveloper.shop/profile)
+#    fi
+#
+#    if [ ${CURRENT_PROFILE} == real1 ]
+#    then
+#      IDLE_PROFILE=real2
+#    else
+#      IDLE_PROFILE=real1
+#    fi
 
     echo "${IDLE_PROFILE}"
 }
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -14,7 +14,7 @@ if [ -z ${IDLE_PID} ]
 then
   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-  echo "> kill -9 $IDLE_PID"
-  sudo kill -9 ${IDLE_PID}
+  echo "> kill -15 $IDLE_PID"
+  sudo kill -15 ${IDLE_PID}
   sleep 5
 fi
